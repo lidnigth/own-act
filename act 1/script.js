014c3cd6b338a749d342ab19042b827bb6b0c347
lidia <lidi4silva0@gmail.com>
2025-09-03 14:56:39 -0300

    calc fim

--- act 1/script.js
+++ act 1/script.js
@@ -2,11 +2,20 @@ function calcular() {
     const preco = parseFloat(document.getElementById("preco").value);
     const desconto = parseFloat(document.getElementById("desconto").value);
 
+    if (desconto < 0 || desconto > 100) {
+        document.getElementById("resultado").innerText = "Preencha corretamente!";
+        document.getElementById("resultado").classList.add("erro");
+        return;
+    } 
+    
     if (isNaN(preco) || isNaN(desconto)) {
-        document.getElementById("resultado").innerText = "Preencha corretamente";
+        document.getElementById("resultado").innerText = "Preencha corretamente!";
+        document.getElementById("resultado").classList.add("erro");
         return;
     }
 
+    document.getElementById("resultado").classList.remove("erro");
+    
     const valorFinal = preco - (preco * desconto / 100);
     const economizz = preco - valorFinal
     document.getElementById("resultado").innerText = `Preço com desconto: R$ ${valorFinal.toFixed(2)}`;
