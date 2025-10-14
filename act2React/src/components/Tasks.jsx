d8e8c1aa1fbea72c0bb672fb431932505c457c9f
lidia <lidi4silva0@gmail.com>
2025-10-14 16:35:42 -0300

    lista de tarefas

--- act2React/src/components/Tasks.jsx
+++ act2React/src/components/Tasks.jsx
@@ -1,9 +1,10 @@
 function Tasks(props) {
   return (
-    <ul>
+    <ul className="ul">
       {props.tasks.map((task) => (
         <li key={task.id} className="listinha">
           {task.title}
+          <button>Ver detalhes</button>
         </li>
       ))}
     </ul>
